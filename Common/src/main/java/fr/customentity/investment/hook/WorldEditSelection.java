db742fba82128490260b4964c9b551cf66e7192a
CustomEntity <customentity@gmail.com>
2021-01-23 22:20:43 +0100

    Source code reset for 2.0

--- Common/src/main/java/fr/customentity/investment/hook/WorldEditSelection.java
+++
@@ -1,9 +0,0 @@
-package fr.customentity.investment.hook;
-
-import org.bukkit.Location;
-import org.bukkit.entity.Player;
-
-public interface WorldEditSelection {
-    Location getMinimumPoint(Player player);
-    Location getMaximumPoint(Player player);
-}
